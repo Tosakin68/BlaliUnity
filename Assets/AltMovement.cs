27ec32a32df4462f23566d075795d88786eff917
Tosakin68 <120755895+Tosakin68@users.noreply.github.com>
2022-12-19 11:55:25 +0100

    Added Tilesets

--- Assets/AltMovement.cs
+++ Assets/AltMovement.cs
@@ -105,7 +105,7 @@ public class AltMovement : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.name.StartsWith("[g]"))
+        if (collision.gameObject.name.EndsWith("Ground"))
         {
             isGrounded = true;
         }
@@ -113,7 +113,7 @@ public class AltMovement : MonoBehaviour
 
     private void OnCollisionExit2D(Collision2D collision)
     {
-        if (collision.gameObject.name.StartsWith("[g]"))
+        if (collision.gameObject.name.EndsWith("Ground"))
         {
             isGrounded = false;
         }
@@ -121,7 +121,7 @@ public class AltMovement : MonoBehaviour
 
     private void OnCollisionStay2D(Collision2D collision)
     {
-        if (collision.gameObject.name.StartsWith("[g]"))
+        if (collision.gameObject.name.EndsWith("Ground"))
         {
             isGrounded = true;
         }
